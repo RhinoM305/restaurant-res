52cb94e6b8a41e352e8fd226e4aacbe3467154eb
Ryan <morenoryan33@gmail.com>
2023-03-04 05:39:39 -0500

    added moment.js to handle date compairsons

--- src/back-end/src/reservations/reservations.controller.js
+++ src/back-end/src/reservations/reservations.controller.js
@@ -2,6 +2,7 @@
  * List handler for reservation resources
  */
 const formatDateNow = require("../utils/format-date");
+const moment = require("moment");
 const service = require("./reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const hasProperties = require("../errors/hasProperties");
@@ -81,102 +82,74 @@ async function reservationsExistUsingDateOrPhone(req, res, next) {
 
 function hasValidDate(req, res, next) {
   const { data = {} } = req.body;
-  // the date variable holds the date from the client and converts the date to be,
-  // UTC that way it can be compaired. If this is not done we will be compairing a date,
-  // from EST time which is 5 hours behind UTC with UTC which is ahead.
-  let date = new Date(`${data.reservation_date} 00:00:00 UTC`);
-  // today is set to 00:00:00 or the start of that day to make it level with
-  // the date from the client. I was running into incossitent compairsons with,
-  // dates because the client was returning a EST timezone date while the server,
-  // is on UTC.
-  let today = new Date(`${formatDateNow()} 00:00:00 UTC`);
-  //we are assuming that the store closes at midnight. or 12am est
 
-  // if (date < today) {
-  //   errors.push({ field: "future", message: "future" });
-  // } else if (date.getDay() + 1 == 2) {
-  //   errors.push({ field: "closed", message: "closed" });
-  // } else {
-  //   if (errors) {
-  //     res.status(400).json({ errors });
-  //   } else {
-  //     next();
-  //   }
-  // }
+  let date = new Date(`${data.reservation_date}`).toJSON().slice(0, 10);
+
+  let today = moment().format().slice(0, 10);
 
   if (date >= today) {
-    // date + 1 to allign date with western hemisphere date, if confused lookup getDay() returns wrong value
-    if (date.getDay() + 1 == 2) {
+    if (moment(date).format("dddd") === "Tuesday") {
       next({
         status: 400,
-        message: `closed`,
+        message: "closed",
       });
     } else next();
   } else {
     next({
       status: 400,
-      message: `future: ${date} and ${today}`,
+      message: `future`,
     });
   }
 }
 
 function hasValidTime(req, res, next) {
   const { data = {} } = req.body;
-
-  const reservationSubmitted = new Date(
-    `${data.reservation_date} ${data.reservation_time} UTC `
-  );
-
-  const reservationTime = reservationSubmitted.toJSON().slice(11, 19);
-
-  const currentTime = new Date().toJSON().slice(11, 19);
-
-  const today = new Date(`${formatDateNow()} ${currentTime} UTC`).toJSON();
-  console.log(today);
-  console.log(reservationSubmitted);
-
-  const ifToday = () => {
-    if (today.slice(0, 10) === data.reservation_date) {
-      return true;
+  //formatting time to always be in 00:00:00 format
+  const reservationTime = () => {
+    if (data.reservation_time.length === 5) {
+      return `${data.reservation_time}:00`;
+    } else {
+      return data.reservation_time;
     }
   };
-  //ifToday checks if reservation date is equal to todays date.
+  const today = moment().format().slice(0, 10);
+  const reservationDate = data.reservation_date;
+  const currentTime = moment().format(`hh:mm:ss`);
+  console.log(reservationTime());
 
-  // the store opens at 10:30am but since we converted to UTC time
-  // the store opens at 3:30pm since UTC is 5 hours ahead of eastern timezone.
+  // 1030 10:30AM OPEN
+  // 2130 9:30PM RESERVATION CUTOFF
+  // 1030 10:30PM CLOSE
+  // 0000 12:00AM MIDNIGHT
 
-  // the store closes at 10:30pm but since we converted to UTC time
-  // the store closes at 3:30am since UTC is 5 hours ahead of eastern timezone.
+  //Checks if before open but after midnight
 
-  // CANNOT MAKE RESERVATIONS WITHIN 1 HOUR OF CLOSING!!!!
-
-  // 1530 10:30AM/3:30PM UTC OPEN
-  // 0230 9:30PM/2:30AM UTC RESERVATION CUTOFF
-  // 0330 10:30PM/3:30AM UTC CLOSE
-  // 0500 12:00AM/5:00AM UTC MIDNIGHT
-
-  //Checks if after midnight but before opening
-  console.log(reservationTime);
-  if (reservationTime < "15:30:00" && reservationTime > "05:00:00") {
+  if (reservationTime() < "10:30:00" && reservationTime() > "00:00:00") {
     next({
       status: 400,
-      message: `We open at 10:30am!!!: ${reservationSubmitted} and ${today}`,
+      message: `We open at 10:30am!!!`,
     });
     //Checks if after cutoff but before closing
-  } else if (reservationTime >= "02:30:00" && reservationTime < "03:30:00") {
+  } else if (
+    reservationTime() >= "21:30:00" &&
+    reservationTime() < "22:30:00"
+  ) {
     next({
       status: 400,
       message: `Sorry, we are not allowed to schedule a reservation within an hour before closing!`,
     });
     //Checks after closing but before midnight
-  } else if (reservationTime > "03:30:00" && reservationTime <= "05:00:00") {
+  } else if (
+    (reservationTime() > "22:30:00" && reservationTime() < "24:00:00") ||
+    reservationTime() === "00:00:00"
+  ) {
     next({
       status: 400,
       message: `Sorry, but we are closed at that time!`,
     });
     //Finally if it is today we compare real time with reservation time
-  } else if (ifToday()) {
-    if (today < new Date()) {
+  } else if (today === reservationDate) {
+    if (reservationTime() < currentTime) {
       next({
         status: 400,
         message: `Sorry, this time is no longer available.`,
--- src/front-end/src/utils/api.js
+++ src/front-end/src/utils/api.js
@@ -73,7 +73,6 @@ export async function getAllReservationDates(signal) {
 }
 
 export async function createReservation(data, signal) {
-  console.log(data);
   const url = `${API_BASE_URL}/reservations`;
   const options = {
     method: "POST",
